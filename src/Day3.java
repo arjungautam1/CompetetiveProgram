34f33e4e4ea9ba8eb5f626317b05d50adcef5c5d
Laserarjun876 <laserarjun876@gmail.com>
2019-07-25 18:55:01 +0545

    Day 3 channlege completed

---
+++ src/Day3.java
@@ -0,0 +1,13 @@
+//A chef was cooking something special which required exact 200ml water but he doesn't have 200ml cup 
+//he has a 500 ml cup and 
+//300m lcup but that dish required exactly 200ml water .
+public class Day3 {
+public static void main(String[] args) {
+	
+	int smallCup=300;
+	int bigCup=500;
+	int requiredWater=bigCup-smallCup;
+	System.out.println("Required Amount of Water is "+requiredWater+"ml");
+	
+}
+}
