ec5273ee4b7ec9c61c64d1418731728e67dc309d
Laserarjun876 <laserarjun876@gmail.com>
2019-07-29 04:41:14 +0545

    day 5 added

--- src/Day4.java
+++ src/Day4.java
@@ -1,4 +1,8 @@
-
+//Jack bought 400 hot dogs for the school picnic .If they were contained in packages of 8 hot dogs 
+//How many total packages did he buy ?
+//Create a java program without using \ or % operator
 public class Day4 {
-
+public static void main(String[] args) {
+	
+}
 }
---
+++ src/Day5.java
@@ -0,0 +1,21 @@
+//During the last week of track trainging. Shoshanna achieves the following times in sec rounds 
+//66,57,54,53,64,52 and 59
+//Found her best score use bubble sort .
+
+public class Day5 {
+public static void main(String[] args) {
+	int array[]= {66,57,54,53,64,52,59};
+	int max=array[0];
+	int i = 0;
+	for(i=0;i<array.length;i++)
+	{
+	if(array[i]>max)
+	{
+		max=array[i];
+		i++;
+	}
+	}
+	System.out.println("Shoshanna achieves the best score of  :"+max);
+}
+
+}
